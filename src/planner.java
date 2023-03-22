5db560c79e4b704c264684dcceaaddcb24f411d5
Sean de Graaf <sean@codegorilla.nl>
2023-03-22 15:24:14 +0100

    Now with functioning text field, button and jspinners.

--- src/planner.java
+++ src/planner.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class planner {
+public class planner extends JFrame {
     private JLabel name;
     private JPanel panelMain;
     private JTextField textName;
@@ -12,4 +14,26 @@ public class planner {
     private JTextArea textAppointments;
     private JSpinner spinnerHour;
     private JSpinner spinnerMinute;
+public planner() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setContentPane(panelMain);
+    this.pack();
+
+    addPerson.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = textName.getText();
+            SpinnerNumberModel hourModel = new SpinnerNumberModel(0, 0, 23, 1);
+            spinnerHour = new JSpinner(hourModel);
+            int hour = (int) spinnerHour.getValue();
+
+            textAppointments.setText(name +" at "+ hour);
+        }
+    });
+}
+
+    public static void main(String[] args) {
+        JFrame frame = new planner();
+        frame.setVisible(true);
+    }
 }
