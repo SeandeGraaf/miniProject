1567f20515110c77ab2c6d6472c326327a8d3fb8
Sean de Graaf <sean@codegorilla.nl>
2023-03-22 11:17:51 +0100

    Initial commit Het prille begin.

---
+++ src/planner.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class planner {
+    private JLabel name;
+    private JPanel panelMain;
+    private JTextField textName;
+    private JLabel timeSlot;
+    private JLabel hour;
+    private JLabel minute;
+    private JTextArea textMessage;
+    private JButton addPerson;
+    private JTextArea textAppointments;
+    private JSpinner spinnerHour;
+    private JSpinner spinnerMinute;
+}
