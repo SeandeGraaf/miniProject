394bd4e1b2f728996ae086272a6e8bf0fe6f0a32
Sean de Graaf <sean@codegorilla.nl>
2023-03-23 16:46:02 +0100

    On the road to recovery.

--- src/planner.java
+++ src/planner.java
@@ -13,6 +13,18 @@ public class planner extends JFrame {
     private JTextArea textMessage;
     private JButton buttonAddNameDate;
     private JTextArea textAppointments;
+
+    public planner(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panelMain);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new planner("Appoitnment planner");
+        frame.setVisible(true);
+    }
 }
 
 
