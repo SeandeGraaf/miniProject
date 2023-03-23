b962a44b38d2391014dee91985a62a2003b34a3c
Sean de Graaf <sean@codegorilla.nl>
2023-03-23 15:26:07 +0100

    Reset on the whole thing.

--- src/planner.java
+++ src/planner.java
@@ -1,49 +1,13 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class planner extends JFrame {
-    private JLabel name;
     private JPanel panelMain;
-    private JTextField textName;
-    private JLabel timeSlot;
-    private JLabel hour;
-    private JLabel minute;
+    private JLabel labelName;
+    private JTextField textFieldNaam;
+    private JLabel labelUur;
+    private JSpinner spinner1;
+    private JLabel labelMinuut;
+    private JSpinner spinner2;
     private JTextArea textMessage;
-    private JButton addPerson;
-    private JTextArea textAppointments;
-    private JSpinner spinnerHour;
-    int theHour;
-    private JSpinner spinnerMinute;
-    int theMin;
-
-    public planner() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(panelMain);
-        createUIComponents();
-
-        addPerson.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String name = textName.getText();
-                theHour = (int) spinnerHour.getValue();
-                theMin = (int) spinnerMinute.getValue();
-                textAppointments.setText(name + " at " + theHour + ":" + theMin);
-            }
-        });
-
-        this.pack();
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new planner();
-        frame.setVisible(true);
-    }
-
-    private void createUIComponents() {
-        SpinnerNumberModel spinnerHour = new SpinnerNumberModel(0, 0, 23, 1);
-        theHour = (int) spinnerHour.getValue();
-
-        SpinnerNumberModel spinnerMinute = new SpinnerNumberModel(0, 0, 59, 1);
-        theMin = (int) spinnerMinute.getValue();
-        }
-}
+    private JButton buttonAddNameDate;
+    private JTextArea textArea1;
