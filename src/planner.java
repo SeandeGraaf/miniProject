6f5b0246a71b5691d6169d63d7b381c8138ba4ee
Sean de Graaf <sean@codegorilla.nl>
2023-03-24 10:02:43 +0100

    Name and time goes into the text field.

--- src/planner.java
+++ src/planner.java
@@ -7,9 +7,9 @@ public class planner extends JFrame {
     private JLabel labelName;
     private JTextField textFieldNaam;
     private JLabel labelUur;
-    private JSpinner spinner1;
+    private JSpinner spinUur;
     private JLabel labelMinuut;
-    private JSpinner spinner2;
+    private JSpinner spinMin;
     private JTextArea textMessage;
     private JButton buttonAddNameDate;
     private JTextArea textAppointments;
@@ -18,17 +18,22 @@ public class planner extends JFrame {
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(panelMain);
+        SpinnerNumberModel uurModel = new SpinnerNumberModel(0, 0, 23, 1);
+        SpinnerNumberModel minModel = new SpinnerNumberModel(0, 0, 59, 1);
+        spinUur.setModel(uurModel);
+        spinMin.setModel(minModel);
         this.pack();
         buttonAddNameDate.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String appointment = String.valueOf(textFieldNaam.getText());
-                textAppointments.setText(appointment);
-
+                int uur = (int) spinUur.getValue();
+                int minuut = (int) spinMin.getValue();
+                textAppointments.setText(String.format("%s om %02d:%02d", appointment, uur, minuut));
             }
         });
     }
-//Testing Github
+
     public static void main(String[] args) {
         JFrame frame = new planner("Appointment planner");
         frame.setVisible(true);
